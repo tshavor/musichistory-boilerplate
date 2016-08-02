991094393cec5430928f05cddcd7a663dea562cb
Thomas J. Shavor <thomasshavor@gmail.com>
2016-08-02 14:13:22 -0500

    finished product

---
+++ songs.js
@@ -0,0 +1,46 @@
+console.log ("songs loaded!");
+
+var songs = [];
+// index 0 of the array
+songs[songs.length] = "Back in Black > by AC/DC on the album Back in Black";
+songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+songs[songs.length] = "Paranoid > by Black Sabbath on the album Paranoid";
+
+// Each student must add one song to the beginning and the end of the array.(DONE)
+
+
+// Loop over the array and remove any words or characters that obviously don't belong.
+var removeChar= [];
+for (var i = 0; i<songs.length; i++) {
+		removeChar.push(songs[i].replace(/[^a-z0-9\s\>]/gi, ""))
+		console.log(removeChar);
+}
+
+// Students must find and replace the > character in each item with a - character.
+var finalArray= [];
+for (var i = 0; i<songs.length; i++) {
+finalArray.push(removeChar[i].replace (/>/g,"-"))
+// for finalArray.push(removeChar[i].replace(">","-"))
+var finalSongs=document.getElementById("right-side");
+finalSongs.innerHTML+= `<p> ${finalArray[i]}</p>`
+		console.log("finalArray", finalArray);
+	}
+
+// Must add each string to the DOM in index.html in the main content area.
+//
+
+
+
+// var phrase = "Hey, look at me!";
+// var element = document.getElementById("right-side");
+// element.innerHTML = phrase;
+
+
+
+
+
+
